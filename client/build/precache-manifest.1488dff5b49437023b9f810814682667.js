a48f6226854cdb8e3b6296f1f77b76822a877bc3
Malakian75s <malakian75s@gmail.com>
2019-03-28 22:47:08 -0600

    build de prueba sin ignore

---
+++ client/build/precache-manifest.1488dff5b49437023b9f810814682667.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "a14509a147afe2e6ac8011402e166827",
+    "url": "/static/media/logo.a14509a1.png"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "37a4d0f27fe0017ef2ac",
+    "url": "/static/js/main.37a4d0f2.chunk.js"
+  },
+  {
+    "revision": "800f6dacd5b0ce78c9dc",
+    "url": "/static/js/2.800f6dac.chunk.js"
+  },
+  {
+    "revision": "37a4d0f27fe0017ef2ac",
+    "url": "/static/css/main.15dbbc41.chunk.css"
+  },
+  {
+    "revision": "800f6dacd5b0ce78c9dc",
+    "url": "/static/css/2.d4836f38.chunk.css"
+  },
+  {
+    "revision": "fe90f91ed6cbe07153688cee59643532",
+    "url": "/index.html"
+  }
+];
