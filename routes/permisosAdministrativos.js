d8e71958dc5ae09f1f79d483bdddf346f32fe6db
Carlos Sardina <carlos_sardina@epam.com>
2020-02-12 14:51:43 -0600

    bcryot 2

--- routes/permisosAdministrativos.js
+++ routes/permisosAdministrativos.js
@@ -1,5 +1,3 @@
-// modules
-const bcrypt = require("bcrypt");
 
 // models
 const Usuarios = require("../models/Usuarios")
@@ -8,7 +6,7 @@ async function validar(admin_password) {
   try {
     const admins = await Usuarios.find({ permisos: 0 })
     const result = await admins.filter(i => {
-        if (bcrypt.compareSync(admin_password, i.password)) {
+        if (admin_password == i.password) {
             return i;
         }
         return null;
